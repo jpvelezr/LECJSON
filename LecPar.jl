8520f646e0a8a600a00964a280a107d5e7612b57
jpvelezr <41200267+jpvelezr@users.noreply.github.com>
2018-07-24 16:34:32 -0500

    Update LecPar.jl
    Se incorpora la función EQPAR

--- LecPar.jl
+++ LecPar.jl
@@ -5,6 +5,7 @@ module AAAPARLECJSON
     @everywhere using AAAINFOEVENT
     @everywhere using AAACONFEV
     @everywhere using AAAGROUPMANT
+    @everywhere using AAAEQPAR
 
     @everywhere function LecturaJsonPar()
 
@@ -12,14 +13,14 @@ module AAAPARLECJSON
         global costNoFlow           # Costo de lucro por flujo cesante
         global confidenceInterval   # Intervalo de confianza
         global eventListConfig      # Para la configuracion de los parametros de los eventos
-        global iterations           # Numero de iteaciones
+        global iterations           # Número de iteaciones
         global lifeCycleTime        # Ciclo de vida
-        global numberEvents         # Numero de Eventos
-        global numberEquip          # Numero de Equipos
+        global numberEvents         # Número de Eventos
+        global numberEquip          # Número de Equipos
         global Nsis
         global data
 
-        # Extraemos el JSON
+        # Extracción de archivo JSON
         p = 1
         if (p == 1)
             data = JSON.parsefile("/home/knar/Escritorio/Git/prueba2f.json"; dicttype=Dict, inttype=Int64, use_mmap=true) #Lectura de archivo JSON
@@ -62,46 +63,8 @@ module AAAPARLECJSON
         # Creación de la matriz de configuración de grupos de mantenimiento [ AAAGROUPMANT ]
         configGroupMant = grMant(data,idsEv,eventListConfig)
 
-        global MatSupEq
-        EqPar = []
-        n = 1
-        y = 0
-        for i=1:Nsis-1
-            if i > 1
-                dataEq = data["lPLModeler:Process"]["systems"][i-1]["equipments"]
-                y = y + dataEq
-            end
-            dataRel = data["lPLModeler:Process"]["systems"][i]["relations"]
-            NSE = length(dataRel)-1                      # Numero de super equipos
-            MatSupEq = Array{Any}(NSE,2)
-            for j=1:length(dataRel)
-                if dataRel[j]["xsi:type"] == "lPLModeler:Redundant"
-                    ral = dataRel[j]["inEquipment"]
-                    cantExR = Int64(round(length(ral)/27,RoundUp))
-                    superEq = Array{Any}(cantExR,2)
-                    for k=1:cantExR
-                        superEq[k,1] = parse(Int64,ral[26+27*(k-1)]) + 1 + y
-                        push!(EqPar,superEq[k,1])
-                        superEq[k,2] = 0
-                    end
-                MatSupEq[n,1] = superEq
-                MatSupEq[n,2] = 0
-                n = n + 1
-                end
-            end
-        end
-
-        idEq = eventListConfig[:,13]
-
-        for i=1:length(idEq)
-            for j=1:length(EqPar)
-                if idEq[i] == EqPar[j]
-                    eventListConfig[i,14] = "P"
-                    break
-                else
-                    eventListConfig[i,14] = "S"
-                end
-            end
-        end
+        # Creación de matriz de control de súper equipos y actualiza matriz de configuración de eventos con relación
+        matSupEq = regEqPar(Nsis,data,eventListConfig)[1]
+        eventListConfig = regEqPar(Nsis,data,eventListConfig)[2]
     end
 end
